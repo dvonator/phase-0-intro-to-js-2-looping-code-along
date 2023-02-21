df59401c6a9c527c322cfbe3e5fc331ec87bc752
David <davidvo1992@gmail.com>
2023-02-21 11:51:33 -0600

    commit tree 266d9d3e0661731c547f4a477687f8cf7975f698

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+
+const testArray = ["Lebron James", "Kobe Bryant", "Tom Brady"];
+
+function writeCards(testArray) {
+  const copyOfTestArray = [];
+  for (let i = 0; i < testArray.length; i++) {
+    copyOfTestArray.push(
+      `Thank you, ${testArray[i]}, for the wonderful surprise gift!`
+    );
+    debugger;
+  }
+
+  return copyOfTestArray;
+}
+
+writeCards(testArray);
+
+function countDown(numbers) {
+  while (numbers >= 0) {
+    console.log(numbers--);
+  }
+}
