0924b94cb4921bcbd807ae752efbe5abf3b040ae
David Vo <Davidvo1992@gmail.com>
2023-04-14 18:54:51 -0500

    to submit lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+
+const list = ['Guadalupe', 'Ollie', 'Aki'];
+
+function writeCards(list,event) {
+    let newList = [];
+    for(let i = 0; i < list.length; i++) {
+        
+    newList.push(`Thank you, ${list[i]}, for the wonderful ${event} gift!`)
+     
+    }
+    return newList
+}
+ 
+function countDown(countdown) {
+    while(countdown >= 0) {
+        console.log(countdown--);
+    }
+}
